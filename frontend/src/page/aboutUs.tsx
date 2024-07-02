e2528245f43d8b16af2b6076148281c74bd88bf9
Vangnea <vangnea.meng@gmail.com>
2024-07-02 20:37:35 +0700

    update_about_us

--- frontend/src/page/aboutUs.tsx
+++ frontend/src/page/aboutUs.tsx
@@ -9,8 +9,7 @@ const AboutUsComponent = () => {
           <div>
             <h1 className="text-5xl font-bold">Box Office News!</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-
+              about us
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
